c2b9516e2e0ae834bc332e7bfb8cdf577f147df5
Arianna <ariannacedenob@gmail.com>
2026-02-25 07:08:52 -0400

    Aqui se encuentra el desarrollo de la Practica 2 sobre Python

---
+++ Practica2.py
@@ -0,0 +1,9 @@
+"Este codigo es un tipo de calculadora"
+
+print ('Holaaa, bienvendio a la parte 2 del python, aqui una suma con valores predefinidos por mi :)')
+
+a= 2
+b= 4
+suma = a + b
+
+print (f'La suma de estos numeros tiene como resultado= {suma}')
